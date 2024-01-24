ac363e0c10e9d0ea92be163628a40cec68e893b9
Yashkanekar <kanekaryash.11@gmail.com>
2024-01-25 00:50:20 +0530

    feat: configured and added redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+import { persistor, store } from "./redux/store.js";
 import App from "./App.jsx";
 import "./index.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor} loading={null}>
+      <App />
+    </PersistGate>
   </Provider>
 );
-
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './user/userSlice';
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+}); // combining all the slices to create one single rootReducer which manages the entire app state
+
+const persistConfig = {
+  key: "root", // name of the key which will be saved with info in the localStorage
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer); // makes our rootReducer persist acc to the config provided
 
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer, // adding the persisted reducer as the main and only reducer
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store); // make our store persist and this persistor will be passed as a prop to the persistGate wrapper which wraps the entire app
